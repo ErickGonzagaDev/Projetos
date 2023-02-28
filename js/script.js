d772525563606448da7b55b05e0fc9fa3793f8c3
ErickGonzagaDev <erickgonzaga.dev@gmail.com>
2023-02-28 13:47:14 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function menuBurguer() {
+    if (document.getElementById('menu-bar').style.display == 'none') {
+        document.getElementById('menu-bar').style.display = 'block'
+    } else {
+        document.getElementById('menu-bar').style.display = 'none'
+    }
+}
